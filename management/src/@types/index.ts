3fe673f8f1c90c965a2a5c8b4f7858328e316a45
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-01-11 14:24:36 +0100

    Init package

---
+++ management/src/@types/index.ts
@@ -0,0 +1,10 @@
+export { IdentityTypes, IdentityStatus } from "./Identity";
+export type { IdentityInput, Identity } from "./Identity";
+export type { IdentityCommandProps } from "./IdentityCommandProps";
+export type { ProviderUser, SingleIdentityProviderUser } from "./ProviderUser";
+export type {
+  CheckVerificationCodeInput,
+  CheckVerificationCodeResult,
+  SendIdentityVerificationCodeInput,
+  SendIdentityValidationCodeResult,
+} from "./VerificationCode";
