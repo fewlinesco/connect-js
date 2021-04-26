30a5e7bb6919d55c7404846ede6c4f1172113cd6
Oluane <rochepauline6@gmail.com>
2021-04-26 14:45:06 +0200

    changing function to fully accept non signed JWT

--- client/src/utils/generateJWE.ts
+++ client/src/utils/generateJWE.ts
@@ -1,16 +1,26 @@
 import jose from "node-jose";
 
+import { JWTPayload } from "../types";
+
 async function generateJWE(
-  jwtOrJws: string,
+  JWTPayloadOrJWS: JWTPayload | string,
   publicKey: string,
 ): Promise<string> {
   const josePublicKeyForEncryption = await jose.JWK.asKey(publicKey, "pem");
+
+  let JWTOrJWS;
+
+  if (typeof JWTPayloadOrJWS === "string") {
+    JWTOrJWS = Buffer.from(JWTPayloadOrJWS);
+  } else {
+    JWTOrJWS = JSON.stringify(JWTPayloadOrJWS);
+  }
 
   return await jose.JWE.createEncrypt(
     { format: "compact" },
     josePublicKeyForEncryption,
   )
-    .update(Buffer.from(jwtOrJws))
+    .update(Buffer.from(JWTOrJWS))
     .final();
 }
 
