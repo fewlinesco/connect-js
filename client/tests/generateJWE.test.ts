1a7697439fa4be73ef947666101f2c2bbc34ecf8
Oluane <rochepauline6@gmail.com>
2021-04-26 14:45:33 +0200

    adding test case for non signed JWT

--- client/tests/generateJWE.test.ts
+++ client/tests/generateJWE.test.ts
@@ -1,13 +1,15 @@
 import crypto from "crypto";
 
+import { defaultPayload } from "../src/utils/defaultObjects";
 import { generateJWE } from "../src/utils/generateJWE";
 import { generateRS256JWS } from "../src/utils/generateJWS";
 
 describe("generateJWE", () => {
   const RS256JWT = generateRS256JWS();
 
-  test("should generate a JWE", async () => {
+  test("should generate a JWE from a JWS", async () => {
     expect.assertions(1);
+
     const { publicKey: publicKeyForEncryption } = crypto.generateKeyPairSync(
       "rsa",
       {
@@ -24,6 +26,32 @@ describe("generateJWE", () => {
     );
 
     const generatedJWE = await generateJWE(RS256JWT, publicKeyForEncryption);
+
+    expect(generatedJWE.split(".").length).toEqual(5);
+  });
+
+  test("should generate a JWE from a JWT", async () => {
+    expect.assertions(1);
+
+    const { publicKey: publicKeyForEncryption } = crypto.generateKeyPairSync(
+      "rsa",
+      {
+        modulusLength: 2048,
+        publicKeyEncoding: {
+          type: "spki",
+          format: "pem",
+        },
+        privateKeyEncoding: {
+          type: "pkcs8",
+          format: "pem",
+        },
+      },
+    );
+
+    const generatedJWE = await generateJWE(
+      defaultPayload,
+      publicKeyForEncryption,
+    );
 
     expect(generatedJWE.split(".").length).toEqual(5);
   });
