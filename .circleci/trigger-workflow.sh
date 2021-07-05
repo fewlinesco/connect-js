7e0328a264a2642a15724bdadb9e56bf7056d067
Oluane <rochepauline6@gmail.com>
2021-07-05 13:37:23 +0200

    cleaning useless echo

--- .circleci/trigger-workflow.sh
+++ .circleci/trigger-workflow.sh
@@ -4,10 +4,7 @@ package_names="client management popup"
 for name in $changed_folder_names
 do 
   if [[ $package_names =~ (^|[[:space:]])"$name"($|[[:space:]]) ]] ; then
-    echo "this is $name"
     data="{ \"branch\": \"$CIRCLE_BRANCH\", \"parameters\": { \"$name\": true, \"trigger\": false } }"
-
-    echo $data
     curl -X POST -u "${CIRCLE_CI_TOKEN}:" --header 'content-type: application/json' -d "$data" https://circleci.com/api/v2/project/github/fewlinesco/connect-js/pipeline
   fi
 done
